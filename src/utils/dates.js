90f611d8b2555e44ef2bb4eb7d802c6465abdb68
Всеволод <vsevolod@MacBook-Air.local>
2021-09-09 23:19:13 +0500

    fix bug in mock

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,12 +1,12 @@
 import dayjs from 'dayjs';
 import { getRandomInteger } from './random.js';
 
-export const addDaysToDate = (date, daysCount) => dayjs().add(getRandomInteger(date, daysCount), 'days').toDate();
+export const addDaysToDate = (date, daysCount) => dayjs().add(getRandomInteger(date.getDay(), daysCount), 'days').toDate();
 
 export const getFormattedReleaseYear = (date) => dayjs(date).format('YYYY');
 
 export const getFormattedReleaseDate = (date) => dayjs(date).format('DD MMMM YYYY');
 
-export const getFormattedCommentDate = (date) => dayjs(date).format('YYYY/MM/DD hh:mm');
+export const getFormattedCommentDate = (date) => dayjs(date).format('YY/MM/DD hh:mm');
 
 export const sortMoviesByDateDesc = (movieA, movieB) => dayjs(movieB.filmInfo.release.date).diff(dayjs(movieA.filmInfo.release.date));
